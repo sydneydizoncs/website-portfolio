b01e4efe335f72fda141b3de5a79e6c0af9ef48b
sydney dizon <58854069+sydneydizoncs@users.noreply.github.com>
2020-07-20 10:31:17 -0400

    Add files via upload

---
+++ home.js
@@ -0,0 +1,7 @@
+//DOCTYPE: JS
+console.log("Welcome to Sydney's Website, this is the console.");
+
+//Introduction function
+function whoAreYou(){
+  console.log("My name is Sydney! I live in Barnegat, NJ and I am a rising senior!");
+}
